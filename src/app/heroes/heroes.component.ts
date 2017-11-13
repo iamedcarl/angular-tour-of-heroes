cb0c5842e2a9dafd0f193d05f99e9a8ab8dd35fe
Edcarl Adraincem <iamedcarl@users.noreply.github.com>
2017-11-13 14:10:08 -0500

    Create hero class and show hero object

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +8,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstrom'
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  hero = 'Windstrom';
 }
